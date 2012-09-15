357664c3149e47e2ebdf562bd97e771ebf93394f
Justin Beck <justinbeck@mac.com>
2012-09-14 20:57:14 -0400

    Initial Commit

---
+++ Fenu/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Fenu
+//
+//  Created by Justin C. Beck on 9/14/12.
+//  Copyright (c) 2012 Justin C. Beck. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Fenu/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Fenu
+//
+//  Created by Justin C. Beck on 9/14/12.
+//  Copyright (c) 2012 Justin C. Beck. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
