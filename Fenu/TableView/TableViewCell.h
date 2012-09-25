f996b99f8d307c1c18b170b4f91f1a93148f9afa
Justin Beck <justinbeck@mac.com>
2012-09-25 18:41:20 -0400

    adds custom table cell

---
+++ Fenu/TableView/TableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  TableViewCell.h
+//  Fenu
+//
+//  Created by Justin C. Beck on 9/25/12.
+//  Copyright (c) 2012 Justin C. Beck. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewCell : UITableViewCell
+
+@end
---
+++ Fenu/TableView/TableViewCell.m
@@ -0,0 +1,29 @@
+//
+//  TableViewCell.m
+//  Fenu
+//
+//  Created by Justin C. Beck on 9/25/12.
+//  Copyright (c) 2012 Justin C. Beck. All rights reserved.
+//
+
+#import "TableViewCell.h"
+
+@implementation TableViewCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
